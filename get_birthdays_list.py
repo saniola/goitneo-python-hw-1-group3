5e702560204946934cbcd62d0aebdfd09b1145c6
saniola <a.kidanov@gmail.com>
2023-10-28 14:18:57 +0300

    greeting list shoud start from today
    greeting list shoud start from today or monday if today is saturday or sunday

--- get_birthdays_list.py
+++ get_birthdays_list.py
@@ -5,8 +5,19 @@ def get_today():
     return datetime.today().date()
 
 def print_results(birthdays_per_week):
-    for day, names in birthdays_per_week.items():
-        print(f"{day}: {', '.join(names)}")
+    today = get_today()
+    current_weekday = today.weekday()
+
+    for _ in range(7):
+        day = (current_weekday % 7)
+        day_name = today.strftime("%A")
+
+        if day_name in birthdays_per_week:
+            names = birthdays_per_week[day_name]
+            print(f"{day_name}: {', '.join(names)}")
+
+        today += timedelta(days=1)
+        current_weekday += 1
 
 def get_next_week_birthdays(users, birthdays_per_week):
     today = get_today()
@@ -16,16 +27,14 @@ def get_next_week_birthdays(users, birthdays_per_week):
         birthday = user["birthday"].date()
         birthday_this_year = birthday.replace(year=today.year)
         delta_days = (birthday_this_year - today).days
-        birthday_weekday = (today + timedelta(days=delta_days)).strftime("%A")
 
         if 0 <= delta_days <= 6:
-            step = 0
-            if birthday_weekday  == "Saturday":
-                step = 2
-            elif birthday_weekday == "Sunday":
-                step = 1
-            birthday_weekday = (today + timedelta(days=delta_days + step)).strftime("%A")
+            if birthday_this_year.weekday() == 5:
+                delta_days += 2
+            elif birthday_this_year.weekday() == 6:
+                delta_days += 1
 
+            birthday_weekday = (today + timedelta(days=delta_days)).strftime("%A")
             birthdays_per_week[birthday_weekday].append(name)
 
 def get_birthdays_per_week(users):
